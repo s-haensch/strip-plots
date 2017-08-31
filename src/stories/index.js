312390881ac016e34990e8af71f6f97fff1b1393
Steffen@Homer <mail@steffen.haensch.de>
2017-08-31 08:35:58 +0200

    added min/max properties

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@kadira/storybook';
 
 
 storiesOf('Scale', module)
-  .add('simple scale', () => (
+  .add('owned homes', () => (
     <App
       width={800}
       height={120}
@@ -14,7 +14,41 @@ storiesOf('Scale', module)
         right: 30,
         bottom: 40,
       }}
+      min={0}
+      max={100}
       dataPath="./germanCitiesCategories.csv"
       dataKey="ownedHomes"
+    />
+  ))
+  .add('not religious', () => (
+    <App
+      width={800}
+      height={120}
+      margin={{
+        left: 30,
+        top: 30,
+        right: 30,
+        bottom: 40,
+      }}
+      min={0}
+      max={100}
+      dataPath="./germanCitiesCategories.csv"
+      dataKey="notReligious"
+    />
+  ))
+  .add('self-employed', () => (
+    <App
+      width={800}
+      height={120}
+      margin={{
+        left: 30,
+        top: 30,
+        right: 30,
+        bottom: 40,
+      }}
+      min={0}
+      max={20}
+      dataPath="./germanCitiesCategories.csv"
+      dataKey="selfEmployed"
     />
   ));
