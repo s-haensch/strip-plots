850d84ef5ec26b4198ffa7fb0ee3d598be9092a8
Steffen@Homer <mail@steffen.haensch.de>
2017-08-31 08:37:24 +0200

    added transparent rectangle for more mouseover area. style changes for mouseover.

--- src/components/StripSeries.js
+++ src/components/StripSeries.js
@@ -15,24 +15,46 @@ function stripLine(props, datum, index) {
 
 
   return (
-    <line
-      style={{
-        strokeOpacity: datum.city === activeStrip ? 1 : 0.2,
-      }}
-      onMouseOver={function() {
-          mouseOverHandler(
-            scale(datum.ownedHomes),
-            datum.city,
-            `${parseFloat(datum[dataKey]).toFixed(2)}%`
-          )
+    <g>
+      <line
+        style={
+          datum.city === activeStrip ?
+          {
+            strokeOpacity: 1,
+            stroke: "white",
+            strokeWidth: 3,
+          } :
+          {
+            strokeOpacity: 0.25,
+            stroke: "black",
+            strokeWidth: 1,
+          }
         }
-      }
-      onMouseOut={mouseOutHandler}
-      key={index}
-      x1={scale(datum.ownedHomes)}
-      y1={height - margin.bottom - 4}
-      x2={scale(datum.ownedHomes)}
-      y2={height - margin.bottom - (datum.city === activeStrip ? 27 : 24)} />
+        key={index}
+        x1={scale(datum[dataKey])}
+        y1={height - margin.bottom - 4}
+        x2={scale(datum[dataKey])}
+        y2={height - margin.bottom - (datum.city === activeStrip ? 27 : 24)} />
+      <rect
+        x={scale(datum[dataKey]) - 3}
+        y={height - margin.bottom - 28}
+        width={5}
+        height={24}
+        onMouseOver={function() {
+            mouseOverHandler(
+              scale(datum[dataKey]),
+              datum.city,
+              `${parseFloat(datum[dataKey]).toFixed(2)}%`
+            )
+          }
+        }
+        onMouseOut={mouseOutHandler}
+        style={{
+          fill: "rgba(0, 0, 0, 0)",
+          stroke: "none",
+        }} />
+      
+    </g>
   );
 }
 
