0b2569022df37fa871daf000a16becfe6b30688f
Steffen@Homer <mail@steffen.haensch.de>
2017-09-15 15:54:38 +0200

    made StripPlot a functional component

--- src/components/StripPlot.js
+++ src/components/StripPlot.js
@@ -4,112 +4,82 @@ import _ from 'lodash';
 import StripSeries from './StripSeries';
 import Axis from './Axis';
 
+const datumByCity = (city, data) => {
+  return _.find(data, (o) => o.city === city);
+}
 
-class StripPlot extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activeStrip: null,
-      activePosition: null,
-      activeValue: null,
-    };
+function StripPlot(props) {
+  const {
+    dimensions,
+    yOffset,
+    data,
+    dataKey,
+    title,
+    min,
+    max,
+    highlight, 
+    mouseOverHandler,
+    mouseOutHandler
+  } = props,
 
-    const {min, max, dimensions} = props,
-      {width, margin} = dimensions;
-    this.scale = scaleLinear()
-      .domain([min, max])
-      .range([margin.left, width - margin.right]);
-  }
-
-  componentWillReceiveProps(nextProps){
-    if (nextProps.data !== null) {
-      this.setActiveByCityAndData(
-        this.props.highlight,
-        nextProps.data);
-    }
-  }
-
-  setActiveByCityAndData(city, data) {
-    const {dataKey} = this.props;
-    const datum = this.datumByCity(city, data);
-
-    this.setState({
-      activeStrip: city,
-      activePosition: this.scale(datum[dataKey]),
-      activeValue: `${parseFloat(datum[dataKey]).toFixed(2)}%`,
-    });
-  }
+  { margin, width } = dimensions,
   
-  setActiveByCity(city) {
-    this.setActiveByCityAndData(city, this.props.data);
-  }
+  scale = scaleLinear()
+    .domain([min, max])
+    .range([margin.left, width - margin.right]),
 
-  resetActiveCity() {
-    this.setState({
-      activeStrip: null,
-      activePosition: null,
-      activeValue: null,
-    });
-  }
+  datum = datumByCity(highlight, data);
 
-  datumByCity(city, data) {
-    return _.find(data, (o) => o.city === city);
-  }
 
-  render() {
-    const { dimensions, yOffset, data, dataKey, title } = this.props,
-    {margin} = dimensions;
-
-    return (
-      <g
-        transform={`translate(0,${yOffset})`}
+  return (
+    data &&
+    <g
+      transform={`translate(0,${yOffset})`}
+    >
+      <text
+        x={margin.left}
+        y={margin.top}
+        style={{
+          fontFamily: "Calibri, Verdana, sans-serif",
+          fontWeight: "bold",
+          fontSize: 15,
+          stroke: "none",
+          fill: "#222"
+        }}
       >
+        {title}
+      </text>
+      {highlight &&
         <text
-          x={margin.left}
-          y={margin.top}
+          x={scale(datum[dataKey])}
+          y={margin.top + 20}
           style={{
             fontFamily: "Calibri, Verdana, sans-serif",
-            fontWeight: "bold",
-            fontSize: 15,
-            stroke: "none",
-            fill: "#222"
+            fontWeight: "normal",
+            fontSize: 13,
+            textAnchor: "middle",
+            stroke: "none"
           }}
         >
-          {title}
+          {`${highlight}: ${parseFloat(datum[dataKey]).toFixed(2)}`}
         </text>
-        {this.state.activeStrip &&
-          <text
-            x={this.state.activePosition}
-            y={margin.top + 20}
-            style={{
-              fontFamily: "Calibri, Verdana, sans-serif",
-              fontWeight: "normal",
-              fontSize: 13,
-              textAnchor: "middle",
-              stroke: "none"
-            }}
-          >
-            {`${this.state.activeStrip}: ${this.state.activeValue}`}
-          </text>
-        }
+      }
 
-        <Axis
-          dimensions={dimensions}
-          scale={this.scale} />
+      <Axis
+        dimensions={dimensions}
+        scale={scale} />
 
+      <StripSeries
+        data={data}
+        dataKey={dataKey}
+        scale={scale}
+        dimensions={dimensions}
+        activeCity={highlight}
+        mouseOverHandler={mouseOverHandler}
+        mouseOutHandler={mouseOutHandler} />
 
-        <StripSeries
-          data={data}
-          dataKey={dataKey}
-          scale={this.scale}
-          dimensions={dimensions}
-          activeStrip={this.state.activeStrip}
-          mouseOverHandler={this.setActiveByCity.bind(this)}
-          mouseOutHandler={this.resetActiveCity.bind(this)} />
-
-      </g>
-    );
-  }
+    </g>
+  );
 }
 
 export default StripPlot;
