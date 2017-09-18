2583148c02c76339ab008dc213ed22dd4be7a033
Steffen@Homer <mail@steffen.haensch.de>
2017-09-18 22:05:51 +0200

    added comments to describe functions

--- src/components/Util.js
+++ src/components/Util.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import { scaleLinear } from 'd3-scale';
 
+/*
+  maps the min and max values of a data column to
+  a defined range (we use a range of 0..1)
+*/
 export const createScale = (data, key, minMaxRange) => {
   return scaleLinear()
     .domain([
@@ -10,6 +14,10 @@ export const createScale = (data, key, minMaxRange) => {
     .range([minMaxRange[0], minMaxRange[1]]);
 };
 
+/*
+  applies the createScale function to a series of column keys
+  of a dataset
+*/
 export const createScales = (data, keys, range) => {
   const scales = {};
   _.map(keys, (key) => {
@@ -19,6 +27,38 @@ export const createScales = (data, keys, range) => {
   return scales;
 };
 
+
+/*
+  maps all values of the given categories to a range 0..1
+  returns an object of objects like
+  {
+    Berlin: {
+      notReligious: 0.234,
+      ownedHomes: 0.540,
+      ...
+    },
+    ...
+  }
+*/
+export const getCityProps = (data, categories) => {
+  const cityProps = {};
+  const scales = createScales(data, categories, [0, 1]);
+
+  _.map(data, (row) => {
+    cityProps[row.city] = {}; // new empty object for each city
+
+    _.map(categories, (category) => {
+      cityProps[row.city][category] = scales[category](row[category]);
+    });
+  });
+
+  return cityProps;
+}
+
+/*
+  applies the createScale function to a series of column keys
+  of a dataset
+*/
 export const getCityMatchValue = (cityProps, otherCityProps, keys) => {
   let sum = 0;
 
@@ -29,16 +69,11 @@ export const getCityMatchValue = (cityProps, otherCityProps, keys) => {
   return Math.sqrt(sum);
 };
 
-export const getBestMatches = (data, compareCity, categories, numberOfMatches) => {
-  const scales = createScales(data, categories, [0, 1]);
-  const cityProps = {};
-  _.map(data, (row) => {
-    cityProps[row.city] = {}; // new empty object for each city
 
-    _.map(categories, (category) => {
-      cityProps[row.city][category] = scales[category](row[category]);
-    });
-  });
+
+export const getBestMatches = (data, compareCity, categories, numberOfMatches) => {
+  
+  const cityProps = getCityProps(data, categories);
 
   const matches = _.map(data, (row) => {
     return {
