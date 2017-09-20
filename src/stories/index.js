7b3c8a923b5d2a463f9b2e1b690f8d286de50c95
Steffen@Homer <mail@steffen.haensch.de>
2017-09-20 16:30:56 +0200

    some cleanup

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,73 +1,70 @@
-import React from 'react';
-import App from '../components/App';
-import { storiesOf } from '@kadira/storybook';
+import React from "react";
+import App from "../components/App";
+import { storiesOf } from "@kadira/storybook";
 
 const baseOptions = {
   suffix: "%",
   midTick: true,
-  format: (datum, dataKey) =>
-    parseFloat(datum[dataKey]).toFixed(2),
+  format: (datum, dataKey) => parseFloat(datum[dataKey]).toFixed(2),
 };
 
-storiesOf('Strip plot', module)
-  .add('All plots', () => (
-    <App
-      width={800}
-      plotHeight={110}
-      margin={{
-        left: 50,
-        top: 50,
-        right: 50,
-        bottom: 50,
-        plotTop: 60,
-        plotBottom: 0,
-      }}
-      dataPath="./germanCitiesCategories.csv"
-      categories={[
-        {
-          key: "notReligious",
-          title: "NON-RELIGIOUS",
-          min: 0,
-          max: 100,
-          options: baseOptions,
+storiesOf("Strip plot", module).add("All plots", () => (
+  <App
+    width={800}
+    plotHeight={110}
+    margin={{
+      left: 50,
+      top: 50,
+      right: 50,
+      bottom: 50,
+      plotTop: 60,
+      plotBottom: 0,
+    }}
+    dataPath="./germanCitiesCategories.csv"
+    categories={[
+      {
+        key: "notReligious",
+        title: "NON-RELIGIOUS",
+        min: 0,
+        max: 100,
+        options: baseOptions,
+      },
+      {
+        key: "ownedHomes",
+        title: "HOME OWNERSHIP",
+        min: 0,
+        max: 100,
+        options: baseOptions,
+      },
+      {
+        key: "selfEmployed",
+        title: "SELF-EMPLOYED",
+        min: 0,
+        max: 20,
+        options: baseOptions,
+      },
+      {
+        key: "populationChange",
+        title: "POPULATION GROWTH 1990‒2014",
+        min: -50,
+        max: 50,
+        options: {
+          suffix: "%",
+          midTick: true,
+          midTickLabel: "no change",
+          format: (datum, dataKey) => parseFloat(datum[dataKey]).toFixed(2),
         },
-        {
-          key: "ownedHomes",
-          title: "HOME OWNERSHIP",
-          min: 0,
-          max: 100,
-          options: baseOptions,
+      },
+      {
+        key: "populationDensity",
+        title: "POPULATION DENSITY",
+        min: 101,
+        max: 4668,
+        options: {
+          subhead: "inhabitants per km²",
         },
-        {
-          key: "selfEmployed",
-          title: "SELF-EMPLOYED",
-          min: 0,
-          max: 20,
-          options: baseOptions
-        },
-        {
-          key: "populationChange",
-          title: "POPULATION GROWTH 1990‒2014",
-          min: -50,
-          max: 50,
-          options: {
-            suffix: "%",
-            midTick: true,
-            midTickLabel: "no change",
-            format: (datum, dataKey) =>
-              parseFloat(datum[dataKey]).toFixed(2),
-          }
-        },
-        {
-          key: "populationDensity",
-          title: "POPULATION DENSITY",
-          min: 101,
-          max: 4668,
-          options: {
-            subhead: "inhabitants per km²",
-          }
-        }
-      ]}
-      highlight="Chemnitz"
-    />
-  ));
+      },
+    ]}
+    highlight="Chemnitz"
+  />
+));
